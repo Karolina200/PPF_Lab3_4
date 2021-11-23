7c42bd97f2ff5db0c979b2bf18f3abd9eb566593
Karolina200 <karolina.ploszaj@student.pk.edu.pl>
2021-11-23 10:26:55 +0100

    commit

---
+++ 02VARIABLES/01var.js
@@ -0,0 +1,24 @@
+var x=4;
+var y=6;
+var z=x+y;
+
+
+var name="Karolina";
+ var person = "Karolina Płoszaj", carName="Volkswagen",price=30000;
+
+ var personA = "Karolina Płoszaj",
+ carNameA = "Volkswagen",
+ priceA=30000;
+
+
+ var carNameB;
+ var personX = "Kasia"+"  "+ "Płoszaj";
+
+ var isFamous = true;//false
+
+ var xx = 2+3+"5";
+ console.log(x,y,z)
+console.log(name)
+console.log(person,personA,personX)
+console.log(carName,carNameA,carNameB)
+console.log(price,priceA)
