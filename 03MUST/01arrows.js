5ce3e981b510f094eb44dd230c2d131251687071
Karolina200 <karolina.ploszaj@student.pk.edu.pl>
2021-11-23 10:40:37 +0100

    commit

---
+++ 03MUST/01arrows.js
@@ -0,0 +1,15 @@
+const getFavouriteNumber =() => 6;
+const addSeven = a => a +7;
+const divide = (a,b) => a / b;
+
+//powyższy kod działa tak samo jak:
+function getFavouriteNumberA() {
+    return 6;
+}
+function addSevenA(a)
+{
+    return a+7;
+}
+function divideA(a,b){
+    return a / b ;
+}
